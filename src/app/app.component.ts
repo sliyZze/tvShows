4bf418e7ae9e9fa867dfe5ade554b0b4d5771d83
„Eren <eren-sivrikaya@hotmail.de>
2024-12-05 08:27:29 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainViewComponent} from './components/main-view/main-view.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tvShows';
+
 }
